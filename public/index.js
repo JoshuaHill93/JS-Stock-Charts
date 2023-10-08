15bc73f71d35168e84fc9de40bbb8aff7e015443
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-10-08 13:23:41 -0700

    added chart js

--- public/index.js
+++ public/index.js
@@ -4,6 +4,31 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+        
+    //apikey=b2b0e11651294ee9a789558a1625b754
+   // ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:BSE&interval=30min&outputsize=24&apikey=b2b0e11651294ee9a789558a1625b754')
+
+    
+    let responce = await fetch ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:BSE&interval=30min&outputsize=24&apikey=e791a4bbb99841ac91f865191f63cbaa')
+    let results =  await responce.json();
+    console.log(results)
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'
+            }]
+        }
+    });
+    
+
+    console.log(stocks[0].values)
+
 }
 
 main()
