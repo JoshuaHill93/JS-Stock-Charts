5abd58e6981dbfb5b80bf5a28e71980e47d36446
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-10-10 17:32:38 -0700

    added getcolor

--- public/index.js
+++ public/index.js
@@ -8,26 +8,52 @@ async function main() {
     //apikey=b2b0e11651294ee9a789558a1625b754
    // ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:BSE&interval=30min&outputsize=24&apikey=b2b0e11651294ee9a789558a1625b754')
 
-    
-    let responce = await fetch ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:BSE&interval=30min&outputsize=24&apikey=e791a4bbb99841ac91f865191f63cbaa')
-    let results =  await responce.json();
-    console.log(results)
+   const { GME, MSFT, DIS, BNTX } = mockData;
 
+   const stocks = [GME, MSFT, DIS, BNTX];
+   function getColor(stock) {
+     if (stock === "GME") {
+       return "rgba(61, 161, 61, 0.7)";
+     }
+     if (stock === "MSFT") {
+       return "rgba(209, 4, 25, 0.7)";
+     }
+     if (stock === "DIS") {
+       return "rgba(18, 4, 209, 0.7)";
+     }
+     if (stock === "BNTX") {
+       return "rgba(166, 43, 158, 0.7)";
+     }
+   }
+
+
+
+
+
+
+    
+   // let responce = await fetch ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX:BSE&interval=30min&outputsize=24&apikey=e791a4bbb99841ac91f865191f63cbaa')
+    //let results =  await responce.json();
+    //console.log(results)
+
+    stocks.forEach( stock => stock.values.reverse())
+
+    // Time Chart
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: stocks[0].values.map(value => value.datetime),
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor:  'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)'
-            }]
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high))
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
         }
     });
     
+    console.log(stocks[0].values)                                                  
 
-    console.log(stocks[0].values)
 
 }
 
